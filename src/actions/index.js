4c26ae9d2c0435b559199a00f0b5b19ef60d819d
Jeff Chung <drifterz303@gmail.com>
2017-01-30 02:53:56 +0900

    reducer와 action를 추가한다

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import { ADD_REMINDER } from '../constants';
+export const addReminder = (text) => {
+  const action = {
+    type: ADD_REMINDER,
+    text
+  }
+  console.log('action in add reminder:', action);
+  return action;
+}
